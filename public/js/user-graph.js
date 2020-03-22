d78db2ec25460cadd433ef494bc9820c35dda93c
[gitavellas] <[situmeang.gita@gmail.com]>
2020-03-23 01:35:43 +0700

    int

---
+++ public/js/user-graph.js
@@ -0,0 +1,74 @@
+//init variable
+var binArray = [];
+var fromuserjson = "";
+var userdata = "";
+var ulength = "";
+var lineChart = "";
+
+function changeChart(year){
+    binArray = [];
+
+    for (var i=1;i<=12;i++){
+        var month = i;
+        if (month<10){
+            month="0"+month
+        }
+        var binactivity = 0;
+        for (var x = 0; x < ulength; x++) {
+            if (fromuserjson[userdata[x]]["month"] == month && fromuserjson[userdata[x]]["year"] == year) {
+                binactivity = binactivity + 1
+            }
+        }
+        binArray.push(binactivity);
+    }
+
+    var ctx = document.getElementById("lineChart");
+    lineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["Jan","Feb","Mar","Apr","Mei","Jun","Jul","Ags","Sep","Oct","Nov","Dec"],
+            datasets: [{ 
+                data: binArray,
+                label:"Sum of Activity",
+                borderColor: "#3e95cd",
+                fill: false
+            }]
+    },
+        options: {
+            title: {
+            display: true,
+            text: "Bin Activity in "+year
+            },
+            scales: {
+                yAxes: [{
+                    scaleLabel: {
+                    display: true,
+                    labelString: 'Sum of Activity'
+                    }
+                }],
+                xAxes: [{
+                    scaleLabel: {
+                    display: true,
+                    labelString: 'Month'
+                    }
+                }]
+            } 
+        }
+    });
+}
+
+var userchart = firebase.database().ref("userBin");
+//get user activity
+userchart.once("value", function(snapshot) {
+    var userjson = JSON.stringify(snapshot);
+    fromuserjson = JSON.parse(userjson);
+    userdata = Object.keys(fromuserjson);
+    ulength = Object.keys(userdata).length;
+
+    changeChart($("#selYear").val())
+});
+
+//deteksi perubahan dropdown year
+$( "#selYear" ).change(function() {
+    changeChart($(this).val());
+});
